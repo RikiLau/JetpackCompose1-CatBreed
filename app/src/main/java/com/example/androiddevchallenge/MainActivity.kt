9a8729a8762e8778d668478df115739e9adb2d1b
riki <moshoubu@sina.com>
2021-03-10 19:27:37 +0800

    加了2只猫

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,12 +18,23 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import androidx.compose.ui.layout.ContentScale
+import com.example.androiddevchallenge.bean.Cat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,10 +48,38 @@ class MainActivity : AppCompatActivity() {
 }
 
 // Start building your app here!
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MyApp() {
+    val list = mutableListOf<Cat>()
+    list.add(Cat("abyssinian", R.drawable.abyssinian))
+    list.add(Cat("american_bobtail", R.drawable.american_bobtail))
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        LazyVerticalGrid (
+            cells = GridCells.Adaptive(minSize = 128.dp)
+                         ) {
+            items(list) { cat ->
+                CatItem(cat)
+            }
+        }
+    }
+}
+
+@Composable
+fun CatItem(cat: Cat) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+          ) {
+        val imageModifier = Modifier
+                .fillMaxWidth()
+        Image(
+            painter = painterResource(id = cat.picRes),
+            contentScale = ContentScale.Crop,
+            modifier = imageModifier,
+            contentDescription = null
+             )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(cat.name)
     }
 }
 
@@ -52,10 +91,10 @@ fun LightPreview() {
     }
 }
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
+/*@Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun DarkPreview() {
     MyTheme(darkTheme = true) {
         MyApp()
     }
-}
+}*/
---
+++ app/src/main/java/com/example/androiddevchallenge/bean/Cat.kt
@@ -0,0 +1,6 @@
+package com.example.androiddevchallenge.bean
+
+data class Cat(
+    val name:String,
+    val picRes: Int
+              )
